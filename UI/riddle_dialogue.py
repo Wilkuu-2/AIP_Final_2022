6c122e2f41c4b2ba12ef5c71a258f5bed3dc53c2
Jakub Stachurski <jakub@wilkuu.xyz>
2023-01-21 22:18:55 +0100

    Add comments to UI code

--- UI/riddle_dialogue.py
+++ UI/riddle_dialogue.py
@@ -1,50 +1,79 @@
+# AI&P Final project [Create M6 2022-2023]
+# UI/riddle_dialogue.py
+#
+# Copyright 2023 Jakub Stachurski
+# Copyright 2023 Natalia Bueno Donadeu
+#
+
+# Imports
 from random import shuffle
 from PyQt5.QtWidgets import QMessageBox, QPushButton
 from input_helpers.input_handler import InHandler
 
 
+# Riddle Dialogue
+#   The a wrappper around the QT QMessageBox that can give a player a riddle
+#   The correctness of the multiple choice answer given by the player
+#   is reported in a event
+#
 class RiddleDialogue():
+    # Constructor
+    #
+    #  main_text -> The riddle
+    #  answers    -> Possible answers for the riddle
+    #  corr_answer -> The index of the correct answer in the answers
+    #  input_handler -> The input handler to send events to
+    #
     def __init__(self, main_text: str, answers: list, corr_answer: int, input_handler: InHandler, bottom_text=None):
-        self.answers = answers
-        self.main_text = main_text
-        self.corr_ans_str = answers[corr_answer]
-        self.corr_ans_button = None
-        self.input_handler = input_handler
-        self.bottom_text = bottom_text
 
         assert len(answers) > 0
 
-        super().__init__()
-
-    def run(self):
+        # Initalize the QMessageBox
         self.message_box = QMessageBox()
-        self.message_box.setText(self.main_text)
+        self.message_box.setText(main_text)
 
-        if self.bottom_text is not None:
-            self.message_box.setInformativeText(self.bottom_text)
+        # Optional text under the riddle
+        if bottom_text is not None:
+            self.message_box.setInformativeText(bottom_text)
 
+        # Buttons held by the class
         self.buttons = []
-        for answer in self.answers:
+
+        self.corr_ans_button = None
+        for i in range(0, len(answers)):
+            answer = answers[i]
+
+            # Initalize the button and add it into buttons
             button = QPushButton(answer)
-            if answer == self.corr_ans_str:
-                self.corr_ans_button = button
             self.buttons.append(button)
 
-        shuffle(self.buttons)
-
-        for button in self.buttons:
-            self.message_box.addButton(button, QMessageBox.AcceptRole)
+            # Save the button that holds the correct answer
+            if i == corr_answer:
+                self.corr_ans_button = button
 
         assert self.corr_ans_button is not None, "Correct answer not found"
 
+        # Attach the buttons to the message box in a random order
+        shuffle(self.buttons)
+        for button in self.buttons:
+            self.message_box.addButton(button, QMessageBox.AcceptRole)
+
+        # Attach the event of clicking
         self.message_box.buttonClicked.connect(self.handle_click)
+
+    # run
+    #   Run opens the dialogue, since the constructor only opens it
+    #
+    def run(self):
         print("OPENING RIDDLE")
         self.message_box.exec()
 
-    async def exec(self):
-        await self.message_box.exec()
-
+    # handle_click
+    #    A QT signal which handles the click of a answer
+    #
     def handle_click(self, button):
         self.input_handler.handle_event("ReleaseHeld")
         self.input_handler.handle_event(
             "Popup_Finish", button == self.corr_ans_button)
+
+    # TODO: Handle the user just killing the popup
--- UI/ui_main.py
+++ UI/ui_main.py
@@ -8,75 +8,119 @@ from game.game_main import Game
 from input_helpers.input_handler import InHandler
 
 
+# UI
+# The main UI Window 
+# 
 class UI(QMainWindow):
+    # Constructor
+    #
     def __init__(self):
-        super().__init__()  # TODO: Initialize UI Here
-        self.pygame = PyGameWidget()
+        super().__init__()  
 
+        # Create the game widget
+        self.pygame = PyGameWidget()
+        
+        # Put the game widget into our window
         layout = QVBoxLayout()
         layout.addWidget(self.pygame, 10)
 
         self.setCentralWidget(QWidget())
         self.centralWidget().setLayout(layout)
 
-        self.setMinimumSize(1024, 576)
+        # Set a minimum size of the window
+        self.setMinimumSize(1024, 1024)
 
+    # keyPressEvent
+    #  QT Signal that gets passed to the game widget 
+    #
     def keyPressEvent(self, event):
         self.pygame.keyPressEvent(event)
         super().keyPressEvent(event)
-
+    
+    # keyReleaseEvent
+    #  QT Signal that gets passed to the game widget 
+    #
     def keyReleaseEvent(self, event):
         self.pygame.keyReleaseEvent(event)
         super().keyReleaseEvent(event)
 
 
+# PyGameWidget 
+# The widget where Pygame gets drawn on
+#
+#
 class PyGameWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    # Constructor
+    #
+    def __init__(self):
+        super().__init__(None)
+        
+        # Initalize the game
         self._input = InHandler()
-
         size = self.frameGeometry().width(), self.frameGeometry().height()
         self.game = Game(size, self._input)
 
         # Game loop is enbedded into the PyQt5 event loop
         self.gameTimer = QTimer()
         self.gameTimer.timeout.connect(self.game.loop)
-        self.gameTimer.start(8)  # 8 ms = 120 fps
-
+        self.gameTimer.start(16)  # 16 ms = 60 fps
+        
+        # The timer to repaint this widget
         self.frameTimer = QTimer()
         self.frameTimer.timeout.connect(self.update)
         self.frameTimer.start(16)  # 16 ms = 60 fps
-
+        
+        # A variable where the game frame is stored 
         self.image = None
 
+    # paintEvent
+    #   A QT Event which paints a new frame of the game 
+    #
+    #
     def paintEvent(self, e):
+        
+        # When a new frame is ready, convert it to a QImage
         if self.game.frame_consume():
             surface = self.game.get_surface()
             w = surface.get_width()
             h = surface.get_height()
             data = surface.get_buffer().raw
             self.image = QImage(data, w, h, QImage.Format_RGB32)
-
+        
+        # When the image exists draw it
         if self.image is not None:
             qp = QPainter(self)
             qp.drawImage(0, 0, self.image)
+        
+        # Pass the event 
         super().paintEvent(e)
-
+    
+    # keyPressEvent
+    #   A QT Event which denotes a keyboard Key being pressed
+    #
     def keyPressEvent(self, event):
         if not event.isAutoRepeat():
             self._input.handle_key(event.key(), "_KeyPress")
         super().keyPressEvent(event)
 
+    # keyPressEvent
+    #   A QT Event which denotes a keyboard Key being released
+    #
     def keyReleaseEvent(self, event):
         if not event.isAutoRepeat():
             self._input.handle_key(event.key(), "_KeyRelease")
         super().keyReleaseEvent(event)
 
+    # keyPressEvent
+    #   A QT Event which denotes the widget being resized
+    #
     def resizeEvent(self, event):
         size = self.frameGeometry().width(), self.frameGeometry().height()
         self.game.resizeEvent(size)
 
-
+# main 
+# The entry point of the UI application
+#
 def main():
     app = QApplication(argv)
 
